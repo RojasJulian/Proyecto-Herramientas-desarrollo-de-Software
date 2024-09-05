e1fb3730ed6a798ffa22306622ceba1baf41c3ed
JulianRojas <julian181099@gmail.com>
2024-09-04 23:26:08 -0500

    initial commit

--- app.py
+++ app.py
@@ -4,33 +4,42 @@ import plotly_express as px
 
 st.header('Venta de vehiculos')
 car_data = pd.read_csv('vehicles_us.csv')  # Leer el dataframe
-# st.write(car_data.head())
+st.write(car_data.head())
 
 
-hist_button = st.button('Crear histograma')  # Creacion de boton
-
+# Botón para el histograma
+hist_button = st.button('Crear histograma')
 
 if hist_button:  # al hacer clic en el botón
-    # escribir un mensaje
-    st.write(
-        'Histograma para el conjunto de datos de anuncios de venta de coches')
+    # Escribir un mensaje
+    st.write('Histograma para el conjunto de datos de anuncios de venta de coches')
 
-    # crear un histograma
-    fig = px.histogram(car_data, x="odometer")
+    fig = px.histogram(car_data,
+                       x="odometer",
+                       nbins=20,  # Número de divisiones
+                       title="Distribución de kilometraje",
+                       labels={"odometer": "Kilometraje (millas)", }
+                       )
 
-    # mostrar un gráfico Plotly interactivo
+    # Mostrar un gráfico Plotly interactivo
     st.plotly_chart(fig, use_container_width=True)
 
 
-scatter_button = st.button('Crear grafico de dispersion')
-
+# Botón para el gráfico de dispersión
+scatter_button = st.button('Crear gráfico de dispersión')
 
 if scatter_button:  # al hacer clic en el botón
-    # escribir un mensaje
+    # Escribir un mensaje
     st.write(
-        'Grafico de dispersion para el conjunto de datos de anuncios de venta de coches')
+        'Gráfico de dispersión para el conjunto de datos de anuncios de venta de coches')
 
-    # crear un histograma
-    graf = px.scatter(car_data, x="odometer", y="price")
+    # Crear un gráfico de dispersión con mejoras
+    graf = px.scatter(car_data,
+                      x="odometer",
+                      y="price",
+                      labels={
+                          "odometer": "Kilometraje (millas)", "price": "Precio (USD)"},
+                      title="Relación entre Kilometraje y Precio",)
 
-    st.plotly_chart(graf)
+    # Mostrar el gráfico de dispersión
+    st.plotly_chart(graf, use_container_width=True)
